c880c269cdf83254bd4522db002627c78b3b3f72
Azat <askazat97@gmail.com>
2017-09-02 01:29:24 +0600

    Added some functionality

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from youtube_dl import YoutubeDL
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET'])
+def get_front_page():
     return render_template('index.html')
 
+@app.route('/getvideo', methods=['POST'])
+def get_video():
+    videoUrl = YoutubeDL().extract_info(request.form['url'], download=False)["requested_formats"][0]["url"]
+    return redirect(videoUrl, code=302)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.debug = True
+    app.run()
